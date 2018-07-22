e3b4ecd88f437ba88b1f0daaa52887806b054c9f
Remco Veldkamp <remcoder@gmail.com>
2018-07-22 17:05:35 +0200

    Created separate milight controller. Used coroutines for all async stuff

--- app/src/main/java/com/q42/milightremotecontrol/MainActivity.kt
+++ app/src/main/java/com/q42/milightremotecontrol/MainActivity.kt
@@ -1,106 +1,42 @@
 package com.q42.milightremotecontrol
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import de.eon.futurelab.utils.UByte
-import de.eon.futurelab.utils.toUnsignedByteArray
-import de.eon.futurelab.utils.unsignedByteArrayOf
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.custom.async
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.launch
 import org.jetbrains.anko.sdk25.coroutines.onClick
-import java.net.DatagramPacket
-import java.net.DatagramSocket
 import java.net.InetAddress
-import java.util.logging.Logger
 
 const val TAG = "MainActivity"
-const val MILIGHT_PORT = 5987
 const val bridge = "hf-lpb100"
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var controller : MiLightController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        launch(CommonPool) {
+            controller = MiLightController(InetAddress.getByName(bridge))
+        }
+
         on_button.onClick {
             Log.i(TAG, "Turning lights on")
-            async() {
-                turnOn(InetAddress.getByName(bridge))
+            launch(CommonPool) {
+                controller.turnOn()
             }
         }
 
         off_button.onClick {
             Log.i(TAG, "Turning lights off")
-            async() {
-                turnOff(InetAddress.getByName(bridge))
+            launch(CommonPool) {
+                controller.turnOff()
             }
         }
     }
 
-    data class Session(val id1 : Int, val id2 : Int)
-    fun requestSession(socket : DatagramSocket, address: InetAddress, port: Int) : Session {
-        val requestSessionCmd = intArrayOf(
-                0x20, 0x00, 0x00, 0x00, 0x16, 0x02, 0x62, 0x3A,
-                0xD5, 0xED, 0xA3, 0x01, 0xAE, 0x08, 0x2D, 0x46,
-                0x61, 0x41, 0xA7, 0xF6, 0xDC, 0xAF, 0xD3, 0xE6,
-                0x00, 0x00, 0x1E) // (use checksum instead?)
-                .toUnsignedByteArray()
-
-        val packet = DatagramPacket(requestSessionCmd ,requestSessionCmd.count(), address, port)
-        // send UDP packet
-        socket.send(packet)
-
-        // receive session id
-        val receiveData = ByteArray(1024)
-        val receivePacket = DatagramPacket(receiveData, receiveData.count())
-        socket.receive(receivePacket)
-        val session1 = UByte.fromByte(receivePacket.data[19])
-        val session2 = UByte.fromByte(receivePacket.data[20])
-        Log.i(TAG, "session id: $session1 $session2")
-        return Session(session1.toInt(), session2.toInt())
-    }
-
-    fun turnOn(address: InetAddress) {
-        // request session
-        val socket = DatagramSocket()
-        val (session1, session2) = requestSession(socket, address, MILIGHT_PORT)
-
-        // wait 50ms
-        Thread.sleep(50)
-
-        // send command using session id
-        val turnLightsOnCmd = unsignedByteArrayOf(
-                0x80,0x00,0x00,0x00,0x11,session1,session2,0x00,0x05,
-                0x00,0x31,0x00,0x00,0x01,0x01,0x07,0x00,0x00,
-                0x00,0x01,0x00,0x3B)
-
-        val packet2 = DatagramPacket(turnLightsOnCmd, turnLightsOnCmd.count(), address, MILIGHT_PORT)
-        socket.send(packet2)
-
-        // Close socket
-        socket.close()
-    }
-
-    fun turnOff(address: InetAddress) {
-        // request session
-        val socket = DatagramSocket()
-        val (session1, session2) = requestSession(socket, address, MILIGHT_PORT)
-
-        // wait 50ms
-        Thread.sleep(50)
-
-        // send command using session id
-        val turnLightsOffCmd = unsignedByteArrayOf(
-                0x80,0x00,0x00,0x00,0x11,session1,session2,0x00,0x05,
-                0x00,0x31,0x00,0x00,0x01,0x01,0x08,0x00,0x00,
-                0x00,0x01,0x00,0x3C)
-
-        val packet2 = DatagramPacket(turnLightsOffCmd, turnLightsOffCmd.count(), address, MILIGHT_PORT)
-        socket.send(packet2)
-
-        // Close socket
-        socket.close()
-    }
 }
---
+++ app/src/main/java/com/q42/milightremotecontrol/MiLightController.kt
@@ -0,0 +1,71 @@
+package com.q42.milightremotecontrol
+
+import android.util.Log
+import de.eon.futurelab.utils.UByte
+import de.eon.futurelab.utils.unsignedByteArrayOf
+import kotlinx.coroutines.experimental.delay
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+
+const val DEFAULT_MILIGHT_PORT = 5987
+
+class MiLightController(private val address: InetAddress, private val port: Int = DEFAULT_MILIGHT_PORT) {
+
+    data class Session(val id1 : Int, val id2 : Int)
+
+    private fun requestSession(socket : DatagramSocket) : Session {
+        val requestSessionCmd = unsignedByteArrayOf(
+                0x20, 0x00, 0x00, 0x00, 0x16, 0x02, 0x62, 0x3A,
+                0xD5, 0xED, 0xA3, 0x01, 0xAE, 0x08, 0x2D, 0x46,
+                0x61, 0x41, 0xA7, 0xF6, 0xDC, 0xAF, 0xD3, 0xE6,
+                0x00, 0x00, 0x1E) // (use checksum instead?)
+
+        val packet = DatagramPacket(requestSessionCmd ,requestSessionCmd.count(), address, port)
+        // send UDP packet
+        socket.send(packet)
+
+        // receive session id
+        val receiveData = ByteArray(1024)
+        val receivePacket = DatagramPacket(receiveData, receiveData.count())
+        socket.receive(receivePacket)
+        val session1 = UByte.fromByte(receivePacket.data[19])
+        val session2 = UByte.fromByte(receivePacket.data[20])
+        Log.i(TAG, "session id: $session1 $session2")
+        return Session(session1.toInt(), session2.toInt())
+    }
+
+    private suspend fun sendCommand(func : (s1:Int, s2:Int)-> ByteArray) {
+        // request session
+        val socket = DatagramSocket()
+        val (session1, session2) = requestSession(socket)
+
+        // wait 50ms
+        delay(50)
+        val cmd = func(session1, session2)
+        // send command using session id
+        val packet2 = DatagramPacket(cmd, cmd.count(), address, port)
+        socket.send(packet2)
+
+        // Close socket
+        socket.close()
+    }
+
+    private fun turnLightsOnCmd(session1: Int, session2 : Int) = unsignedByteArrayOf(
+            0x80,0x00,0x00,0x00,0x11,session1,session2,0x00,0x05,
+            0x00,0x31,0x00,0x00,0x01,0x01,0x07,0x00,0x00,
+            0x00,0x01,0x00,0x3B)
+
+    private fun turnLightsOffCmd(session1: Int, session2 : Int) = unsignedByteArrayOf(
+            0x80,0x00,0x00,0x00,0x11,session1,session2,0x00,0x05,
+            0x00,0x31,0x00,0x00,0x01,0x01,0x08,0x00,0x00,
+            0x00,0x01,0x00,0x3C)
+
+    suspend fun turnOn() {
+       sendCommand(this::turnLightsOnCmd)
+    }
+
+    suspend fun turnOff() {
+       sendCommand(this::turnLightsOffCmd)
+    }
+}
