0b023206090304292d511dcdf2f0a5b91f5714ec
Remco Veldkamp <remcoder@gmail.com>
2018-08-11 23:16:08 +0200

    Prevent crash

--- app/src/main/java/com/q42/milightremotecontrol/MainActivity.kt
+++ app/src/main/java/com/q42/milightremotecontrol/MainActivity.kt
@@ -5,9 +5,12 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
 import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.jetbrains.anko.toast
 import java.net.InetAddress
+import java.net.UnknownHostException
 
 const val TAG = "MainActivity"
 const val bridge = "hf-lpb100"
@@ -21,20 +24,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         launch(CommonPool) {
-            controller = MiLightController(InetAddress.getByName(bridge))
+            try {
+                controller = MiLightController(InetAddress.getByName(bridge))
+            } catch (ex: UnknownHostException) {
+                launch(UI) {
+                    toast("Error while looking for bridge:\n${ex.message}")
+                }
+            }
         }
 
         on_button.onClick {
             Log.i(TAG, "Turning lights on")
             launch(CommonPool) {
-                controller.turnOn()
+                try {
+                    controller.turnOn()
+                } catch(ex: Exception)  {
+                    launch(UI) {
+                        toast("Error turning on lights:\n${ex.message}")
+                    }
+                }
             }
         }
 
         off_button.onClick {
             Log.i(TAG, "Turning lights off")
             launch(CommonPool) {
-                controller.turnOff()
+                try {
+                    controller.turnOff()
+                } catch(ex: Exception)  {
+                    launch(UI) {
+                        toast("Error turning off lights:\n${ex.message}")
+                    }
+                }
             }
         }
     }
